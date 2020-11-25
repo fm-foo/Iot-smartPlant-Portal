75a49ddc9eccf13f35602e319e00c297f29f49af
MrEss-Codes <57900884+MrEss-Codes@users.noreply.github.com>
2020-11-25 22:26:46 +0100

    Initital identity scaffolding
    adds a bunch of stuff, I'm honestly not sure if this is right... I'll most likely revert this.

---
+++ IoT-SmartPlant-Portal/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using IoT_SmartPlant_Portal.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IoT_SmartPlant_Portal.Areas.Identity.IdentityHostingStartup))]
+namespace IoT_SmartPlant_Portal.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IoT_SmartPlant_PortalContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("IoT_SmartPlant_PortalContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IoT_SmartPlant_PortalContext>();
+            });
+        }
+    }
+}
